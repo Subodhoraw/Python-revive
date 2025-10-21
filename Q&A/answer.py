a199df9dbc0af72dd6cd5d248f5cd80b7a54b8c5
orawsuhan <orawsubodh@gmail.com>
2025-10-21 17:25:53 +0530

    we are facing issues with anaconda soon we will fetch that cause our code not updated on the file,

---
+++ Q&A/answer.py
@@ -0,0 +1,7 @@
+## what are list comprehensions? convert a for loop that squares number into alist comprehensions. implemeent squares(n) returning [0...n-1]
+list_squares = []
+list = [1,2,3,4,5]
+for  n in list:
+    list_squares.append(n**2)
+print(list_squares)
+    
