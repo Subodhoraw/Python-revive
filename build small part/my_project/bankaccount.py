fcc968c01af1c4e5f5fb03ee4f20d01a69d1cc88
orawsuhan <orawsubodh@gmail.com>
2025-10-27 01:00:55 +0530

    we are still working oin

--- build small part/my_project/bankaccount.py
+++ build small part/my_project/bankaccount.py
@@ -1,3 +1,11 @@
 class Bankaccount:
     def __init__(self, account_number,balance):
         self.account_number = account_number
+        self.balance = balance
+    def __str__(self):
+        return f"Account ending in {self.account_number[-4:]}: ${self.balance:,.2f}"
+    def __repr__(self):
+        return f"BankAccount(account_number='{self.account_number}',balance ={self.balance})"
+account = Bankaccount("123456789",5420.200)
+print("Your account:",str(account))  
+print("Debug:", repr(account))
