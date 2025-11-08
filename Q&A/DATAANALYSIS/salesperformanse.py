4965880bb328bfaaf78e568cd3d9630196c1cf0d
orawsuhan <orawsubodh@gmail.com>
2025-11-08 23:59:18 +0530

    added

--- Q&A/DATAANALYSIS/salesperformanse.py
+++ Q&A/DATAANALYSIS/salesperformanse.py
@@ -10,12 +10,19 @@ print("="*40)
 
 for person, sales in sales_data.items():
     range_val = max(sales) - min(sales)
-    mean_val = np.means(sales)
+    mean_val = np.mean(sales)
 
     print(f"\n{person}:")
     print(f" Range: ${range_val:,}")
     print(f"Average: ${mean_val:,.0f}")
     print(f" Min: ${min(sales):,}")
     print(f"  Max: $ {max(sales):,}")
-    
+
+    if range_val < 5000:
+        print(f" very consistent")
+    elif range_val < 2000:
+        print(f"  Consistency: Moderately Consistent ⭐⭐")
+    else:
+        print(f"  Consistency: Highly Variable ⭐")
+
 
